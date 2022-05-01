77bcc200dfacd78e561bd8c3e371e9bdcfeffc48
reybritez <stevenreybritez@gmail.com>
2022-05-01 18:11:37 -0400

    añadi las rutas de entrada y formulario

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, requests, flash, redirect, render_template, url_for
+import requests 
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+
+db = SQLAlchemy(app)
+
 
 
 @app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+def inicio():
+    return render_template('index.html')
 
-app.run(host="137.184.32.228", port=80)
+@app.route('/entrada')
+def entrada():
+    return render_template('index.html')
+
+@app.route('/formulario')
+def formulario():
+    return render_template('formulario.html')
+
+app.run(host="137.184.32.228", port=80)     
+
+if __name__ == "__main__":
+    app.run(debug=True)
